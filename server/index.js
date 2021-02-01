82cf94bfd5fff77451944bf0757a9eb3638b3f3c
coyjennings <coy@coyjenningswdp.com>
2021-02-01 15:15:11 -0700

    styling with ant design

--- server/index.js
+++ server/index.js
@@ -1,32 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
+const cors = require('cors')
 
-const path = require('path');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const config = require("./config/key");
 
-const config = require('./config/key');
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect(config.mongoURI, { useNewUrlParser: true })
+//   .then(() => console.log("DB connected"))
+//   .catch(err => console.error(err));
 
-
-//application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
-
-//application/json
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-const mongoose_con = require('mongoose');
-mongoose_con.connect(config.mongoURI, {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB connnected...'))
-  .catch(err => console.log(err))
-
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 app.use(cors())
 
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.use('/api/users', require('./routes/users'));
+
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
@@ -35,7 +43,7 @@ app.use('/uploads', express.static('uploads'));
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 
-  // Set static folder
+  // Set static folder   
   // All the javascript and css files will be read and served from this folder
   app.use(express.static("client/build"));
 
